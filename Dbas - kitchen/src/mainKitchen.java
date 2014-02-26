8a5de8ff6fc5e674fca9a6443936a001e4bb0122
JSandmark <johansandmark@gmail.com>
2014-02-26 20:41:39 +0100

    Updated class info

--- Dbas - kitchen/src/mainKitchen.java
+++ Dbas - kitchen/src/mainKitchen.java
@@ -1,6 +1,12 @@
+/**
+ * Main class for the Dbas lab 1
+ * @author Johan Sandmark, Jesper Norman
+ *
+ */
+
 public class mainKitchen {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-}
+	}
 }
