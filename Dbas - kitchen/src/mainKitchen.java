856a49de605dae2ba018de6b1ebea8801567613a
Jesper <Jesper@Jesper-HP.lan>
2014-02-26 21:05:51 +0100

    hej

--- Dbas - kitchen/src/mainKitchen.java
+++ Dbas - kitchen/src/mainKitchen.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Main class for the Dbas lab 1
  * @author Johan Sandmark, Jesper Norman
@@ -8,5 +10,32 @@ public class mainKitchen {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		Scanner scanner = new Scanner(System.in);
+			
+		
+		boolean running = true;
+		while(running){
+			int menuOption = stringToInt(scanner.nextLine());			
+		}
 	}
+	
+	
+	/**
+	  * Converts a string to an int if possible.
+	  * 
+	  * @param s String to convert.
+	  * @return Converted value. -1 if not possible to convert.
+	  */
+	 private static int stringToInt(String s) {
+	  int num;
+
+	  try {
+	   num = Integer.parseInt(s);
+	  } catch (NumberFormatException e) {
+	   return -1;
+	  }
+
+	  return num;
+	 }
 }
